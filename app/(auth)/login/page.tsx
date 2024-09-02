f0511d148a7b8722dd8a8a13011423899ae98cc9
Sergey <redmoloch@yandex.ru>
2024-09-02 22:57:28 +0300

    fix(mc-next-5-2): Попытка деплоя на версель 5

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { FC } from "react";
 import { requestLogin$ } from "@/shared/clientApi/auth";
 import { AppLink } from "@/shared/components/ui/AppLink";
 import { Form } from "@/shared/components/ui/form/form";
 import { FormInput } from "@/shared/components/ui/form/form-input";
-import { redirectQueryKey } from "@/shared/const/redirectQueryKey";
+// import { redirectQueryKey } from "@/shared/const/redirectQueryKey";
 import { useAppDispatch } from "@/shared/hooks/useAppDispatch";
 import { useAppForm } from "@/shared/hooks/useAppForm";
 import appRoutes from "@/shared/lib/configs/routes/routes";
@@ -21,19 +21,19 @@ const LoginPage: FC = () => {
   const form = useAppForm(LoginSchema);
   const router = useRouter();
   const dispatch = useAppDispatch();
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
 
   const onSubmit = async (data: AnyDict) => {
     try {
-      const redirectPath = searchParams.get(redirectQueryKey)
-        ? `${appRoutes.main}${searchParams.get(redirectQueryKey)}`
-        : appRoutes.main;
+      // const redirectPath = searchParams.get(redirectQueryKey)
+      //   ? `${appRoutes.main}${searchParams.get(redirectQueryKey)}`
+      //   : appRoutes.main;
       const response = await requestLogin$(data as LoginSchemaType);
       const { tokens, ...user } = response;
       dispatch(userActions.setUser(user));
       TokenService.accessToken = tokens.access;
       TokenService.refreshToken = tokens.refresh;
-      router.push(redirectPath);
+      // router.push(redirectPath);
     } catch (e) {
       setFormErrors(e as ApiError, form.setError);
     }
--- app/api/gameResults/scores/route.ts
+++ app/api/gameResults/scores/route.ts
@@ -1,7 +1,7 @@
-import { GameResultService } from "@/shared/lib/db/services/gameResultService";
+// import { GameResultService } from "@/shared/lib/db/services/gameResultService";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const scores = await GameResultService.getScoreList();
-  return NextResponse.json(scores);
+  // const scores = await GameResultService.getScoreList();
+  return NextResponse.json("scores");
 }
